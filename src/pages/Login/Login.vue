653d3a9da43c1fb8f10a458d7b34453283d56854
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-03-01 17:27:09 +0800

    feat: add alertTip component and verify login input

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,45 +4,42 @@
       <div class="login_header">
         <h2 class="login_logo">Foodie Delivery</h2>
         <div class="login_header_title">
-          <a href="javascript:;" >手機登錄</a>
-          <a href="javascript:;" class="on">帳號登錄</a>
+          <a href="javascript:;" :class="{on:loginWay==='text'}" @click="loginWay='text'">簡訊登錄</a>
+          <a href="javascript:;" :class="{on:loginWay==='password'}" @click="loginWay='password'">密碼登錄</a>
         </div>
       </div>
       <div class="login_content">
-        <form>
-          <div >
+        <form @submit.prevent="login">
+          <div :class="{on:loginWay==='text'}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手機號碼" />
-              <button disabled="disabled" class="get_verification">
-                獲取驗證碼
+              <input type="tel" maxlength="11" placeholder="手機號碼" v-model="phone"/>
+              <button :disabled="!phoneCorrect" class="get_verification" :class="{phone_Correct: phoneCorrect}" @click.prevent="getCode">
+                {{countdownTimer > 0 ? `已發送 (${countdownTimer}s)` : '獲取驗證碼'}}
               </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="驗證碼" />
+              <input type="tel" maxlength="8" placeholder="驗證碼" v-model="code"/>
             </section>
             <section class="login_hint">
               温馨提示：若未註冊富迪外送，登錄時將自動註冊，且代表已同意
               <a href="javascript:;">《用戶服務協議》</a>
             </section>
           </div>
-          <div class="on">
+          <div :class="{on:loginWay==='password'}">
             <section>
               <section class="login_message">
-                <input
-                  type="tel"
-                  maxlength="11"
-                  placeholder="手機/郵箱/用户名"
-                />
+                <input type="tel" maxlength="11" placeholder="手機/郵箱/用户名" v-model="name"/>
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码" />
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input type="text" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd"/>
+                <input type="password" maxlength="8" placeholder="密码" v-else v-model="pwd"/>
+                <div class="switch_button" :class="showPwd ? 'on':'off'" @click="showPwd=!showPwd" >
+                  <div class="switch_circle" :class="{go_right: showPwd}"></div>
+                  <span class="switch_text">{{showPwd ? '...':'see'}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="驗證碼" />
+                <input type="text" maxlength="11" placeholder="驗證碼" v-model="captcha"/>
                 <img
                   class="get_verification"
                   src="./images/captcha.svg"
@@ -59,11 +56,86 @@
         <font-awesome-icon class="icon-arrow" icon="fa-solid fa-angle-left" />
       </a>
     </div>
+    <AlertTip :alertText="alertText" v-show="showAlert" @closeTip="closeTip"></AlertTip>>
   </section>
 </template>
 
 <script>
-export default {}
+import AlertTip from '../../components/AlertTip/AlertTip.vue'
+export default {
+  data () {
+    return {
+      loginWay: 'text', // 'text'代表簡訊登錄,'password'代表密碼登錄
+      countdownTimer: 0, // 倒數計時
+      showPwd: false, // 是否顯示密碼
+      phone: '', // 手機號碼
+      name: '', // 用戶名
+      pwd: '', // 密碼
+      code: '', // 簡訊驗證碼
+      captcha: '', // 圖形驗證碼
+      alertText: '', // 提示文本
+      showAlert: false // 是否顯示提示警告框
+    }
+  },
+  components: { AlertTip },
+  computed: {
+    phoneCorrect () {
+      return /^0\d{9}$/.test(this.phone)
+    }
+  },
+  methods: {
+    // 異步獲取驗證碼
+    getCode () {
+      if (!this.countdownTimer) {
+        // 啟動倒數計時
+        this.countdownTimer = 30
+        const intervalId = setInterval(() => {
+          this.countdownTimer--
+          if (this.countdownTimer <= 0) {
+          // 停止計時
+            clearInterval(intervalId)
+          }
+        }, 1000)
+
+      // 發送ajax請求（向指定的手機號碼發送簡訊）
+      }
+    },
+    showAlertWithText (alertText) {
+      this.showAlert = true
+      this.alertText = alertText
+    },
+    // 異步登入
+    login () {
+      // 前台表單驗證
+      if (this.loginWay === 'text') { // 簡訊登陸
+        const { phoneCorrect, code } = this
+        if (!phoneCorrect) {
+          // 提示：手機號碼格式錯誤
+          this.showAlertWithText('手機號碼格式錯誤')
+        } else if (!/^\d{6}$/.test(code)) {
+          // 提示：驗證碼必須為6位數字
+          this.showAlertWithText('驗證碼必須為6位數字')
+        }
+      } else { // 密碼登錄
+        const { name, pwd, captcha } = this
+        if (!name) {
+          // 提示：用戶名必須指定
+          this.showAlertWithText('用戶名必須指定')
+        } else if (!pwd) {
+          // 提示：密碼必須指定
+          this.showAlertWithText('密碼必須指定')
+        } else if (!captcha) {
+          // 提示：驗證碼必須指定
+          this.showAlertWithText('驗證碼必須指定')
+        }
+      }
+    },
+    closeTip () {
+      this.showAlert = false
+      this.alertText = ''
+    }
+  }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -127,6 +199,8 @@ export default {}
               color #ccc
               font-size 14px
               background transparent
+              &.phone_Correct
+                color black
           .login_verification
             position relative
             margin-top 16px
@@ -155,7 +229,6 @@ export default {}
               &.on
                 background #02a774
               >.switch_circle
-                //transform translateX(27px)
                 position absolute
                 top -1px
                 left -1px
@@ -166,6 +239,8 @@ export default {}
                 background #fff
                 box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                 transition transform .3s
+                &.go_right
+                  transform translateX(27px)
           .login_hint
             margin-top 12px
             color #999
